72109d89ea5c130168c649c9dffa926a75e5c3e5
Alex Yarov <alexxd91@gmail.com>
2021-08-25 21:18:25 +0300

    Initial Commit

---
+++ ReusableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReusableView
+//
+//  Created by Alexey Yarov on 25/08/2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
